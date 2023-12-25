85c44d090353e4d8b06d6eb0e5b8e98a1363a4b7
Amirhossein Zandi <blackviper933@gmail.com>
2023-12-25 23:02:10 +0330

    swiper slider

--- components/MainContainer/Slider/Slider.tsx
+++ components/MainContainer/Slider/Slider.tsx
@@ -1,26 +1,133 @@
 "use client"
-
+import "./Slider.css"
 import Image from "next/image";
 import { memo, useState } from "react"
+import pic1 from "../../../public/1.webp"
+import pic2 from "../../../public/2.webp"
+import pic3 from "../../../public/3.webp"
+import pic4 from "../../../public/4.webp"
+import pic5 from "../../../public/5.webp"
+import pic6 from "../../../public/6.webp"
+import pic7 from "../../../public/7.webp"
+import pic8 from "../../../public/8.webp"
+import pic9 from "../../../public/9.webp"
+import pic10 from "../../../public/10.gif"
 import LoadingPic from "../../../public/loading.gif"
+import 'swiper/css/bundle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+
+import SwiperCore from 'swiper';
+
+
 
 
 
 
 const Slider = () => {
-    const [loading, setLoading] = useState(true)
+    const [swiperStatus, setSwiperStatus] = useState<SwiperCore | null>(null);
+
+    const swiperInit = (swiper: SwiperCore) => {
+        setSwiperStatus(swiper);
+    };
+
+
+
+    const handleMouseEnter = () => {
+        if (swiperStatus) {
+            swiperStatus.autoplay.stop();
+        }
+    }
+
+
+    const handleMouseLeave = () => {
+        if (swiperStatus) {
+            swiperStatus.autoplay.start();
+        }
+    }
 
 
 
     return (
-        <section className="slider container mx-auto mt-[60px] lg:mt-[130px]">
-            <figure className="w-full">
-            <Image className={`loading mx-auto ${loading ? "block" : "hidden"}`} priority={true} src={LoadingPic} alt="loading" width={500} height={500} />
+        <section className="slider container mx-auto mt-[60px] lg:mt-[130px]" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <Swiper onSwiper={swiperInit}
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                autoplay={{
+                    delay: 5000
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation, Autoplay]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic1} alt="pic1" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
 
-            <Image src="https://dkstatics-public.digikala.com/digikala-adservice-banners/a9af4d27083f39d25d7b84c77660071afbc22802_1701632316.jpg?x-oss-process=image/quality,q_95/format,webp" priority={true} alt="slider" width={1262} height={300} onLoad={() => setTimeout(() => setLoading(false), 2000)} className={loading ? "hidden" : "block"}/>
-            </figure>
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic2} alt="pic2" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic3} alt="pic3" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic4} alt="pic4" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic5} alt="pic5" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic6} alt="pic6" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic7} alt="pic7" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic8} alt="pic8" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic9} alt="pic9" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <figure>
+                        <Image src={pic10} alt="pic10" width={1272} height={300} />
+                    </figure>
+                </SwiperSlide>
+
+            </Swiper>
         </section>
     );
 }
+
 
 export default memo(Slider);
