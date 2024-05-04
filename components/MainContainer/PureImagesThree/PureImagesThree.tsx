9aeae6a8c40229c6d3c21296b5bcce04a7ef6bf5
Amirhossein Zandi <blackviper933@gmail.com>
2024-05-04 11:03:06 +0330

    "Changed image container layout to grid in PureImagesTwo and PureImagesThree components; Updated border-bottom color in globals.scss"

--- components/MainContainer/PureImagesThree/PureImagesThree.tsx
+++ components/MainContainer/PureImagesThree/PureImagesThree.tsx
@@ -7,13 +7,13 @@ const PureImagesThree = () => {
         <>
             <section className="pure-images-third-part my-8">
                 <div className="container mx-auto px-2">
-                    <div className="flex flex-wrap gap-9 justify-between lg:gap-1">
+                    <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
 
-                        <figure className="overflow-hidden rounded-lg w-full lg:w-[45%]">
+                        <figure className="overflow-hidden rounded-lg">
                             <Image width={452} height={181} src="https://dkstatics-public.digikala.com/digikala-adservice-banners/362824d41f15cee11bfc30c5c31be8bc237ea760_1704528039.jpg?x-oss-process=image/quality,q_95" className="w-full h-full" alt="picture" />
                         </figure>
 
-                        <figure className="overflow-hidden rounded-lg w-full lg:w-[45%]">
+                        <figure className="overflow-hidden rounded-lg">
                             <Image width={452} height={181} src="https://dkstatics-public.digikala.com/digikala-adservice-banners/4f99586add848015000a829457e705916c33c63d_1704800180.jpg?x-oss-process=image/quality,q_95" className="w-full h-full" alt="picture" />
                         </figure>
 
