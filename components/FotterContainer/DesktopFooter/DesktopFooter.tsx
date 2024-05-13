46352b064d5c680425917d06670fb76fb117380b
Amirhossein Zandi <blackviper933@gmail.com>
2024-05-13 16:34:18 +0330

    Add FooterContainer to MainContainer; remove placeholder text from MainContainer
    The diff adds the FooterContainer component to the MainContainer component in the app/page.tsx file. It also removes a large block of placeholder text

---
+++ components/FotterContainer/DesktopFooter/DesktopFooter.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+const DesktopFooter = () => {
+    return (
+        <section className="desktop-footer-container">
+            <div className="container mx-auto px-2">
+                desktop footer
+            </div>
+        </section>
+    );
+}
+
+export default DesktopFooter;
