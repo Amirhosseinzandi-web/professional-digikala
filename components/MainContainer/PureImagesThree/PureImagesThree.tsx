7644cb7133bd225f216a3e1445384d305efba830
Amirhossein Zandi <blackviper933@gmail.com>
2024-05-25 22:14:22 +0330

    "Updated globals.scss, FooterContainer, MobileFooter, PureImagesThree, and PureImagesTwo components with new styles, images, and content."

--- components/MainContainer/PureImagesThree/PureImagesThree.tsx
+++ components/MainContainer/PureImagesThree/PureImagesThree.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import imageOne from "../../../public/landing/pure-images-third-1.webp"
+import imageTwo from "../../../public/landing/pure-images-third-2.webp"
 
 
 const PureImagesThree = () => {
@@ -9,12 +10,12 @@ const PureImagesThree = () => {
                 <div className="container mx-auto px-2">
                     <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
 
-                        <figure className="overflow-hidden rounded-lg">
-                            <Image width={452} height={181} src="https://dkstatics-public.digikala.com/digikala-adservice-banners/362824d41f15cee11bfc30c5c31be8bc237ea760_1704528039.jpg?x-oss-process=image/quality,q_95" className="w-full h-full" alt="picture" />
+                        <figure className="overflow-hidden rounded-lg relative">
+                            <Image width={452} height={181} src={imageOne} className="w-full h-full" alt="picture" />
                         </figure>
 
                         <figure className="overflow-hidden rounded-lg">
-                            <Image width={452} height={181} src="https://dkstatics-public.digikala.com/digikala-adservice-banners/4f99586add848015000a829457e705916c33c63d_1704800180.jpg?x-oss-process=image/quality,q_95" className="w-full h-full" alt="picture" />
+                            <Image width={452} height={181} src={imageTwo} className="w-full h-full" alt="picture" />
                         </figure>
 
                     </div>
--- components/MainContainer/PureImagesTwo/PureImagesTwo.tsx
+++ components/MainContainer/PureImagesTwo/PureImagesTwo.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import imageOne from "../../../public/landing/pure-images-second-1.gif"
+import imageTwo from "../../../public/landing/pure-images-second-2.webp"
 
 
 
@@ -11,11 +12,11 @@ const PureImagesTwo = () => {
                     <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
 
                         <figure className="overflow-hidden rounded-lg">
-                            <Image width={452} height={181} src="https://dkstatics-public.digikala.com/digikala-adservice-banners/792b26d05b1af23cc09a37969d8739caae938b01_1704268606.gif?x-oss-process=image" className="w-full h-full" alt="picture" />
+                            <Image width={452} height={181} src={imageOne} className="w-full h-full" alt="picture" />
                         </figure>
 
                         <figure className="overflow-hidden rounded-lg">
-                            <Image width={452} height={181} src="https://dkstatics-public.digikala.com/digikala-adservice-banners/1b0c30dfbcf60131132f78854caf87e7b0bc30b4_1701265164.jpg?x-oss-process=image/quality,q_95/format,webp" className="w-full h-full" alt="picture" />
+                            <Image width={452} height={181} src={imageTwo} className="w-full h-full" alt="picture" />
                         </figure>
 
                     </div>
